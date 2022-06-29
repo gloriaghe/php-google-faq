77ea2e961d2807c27c0a4305d43b3f1e1f2bdbea
Gloria <gloghe@yahoo.it>
2022-06-29 09:32:42 +0200

    mod ol

--- index.php
+++ index.php
@@ -26,14 +26,14 @@ $arr_domande = [
         "dom" => "Perché il mio account è associato a un paese?",
         "risp" => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
 
-        <ol> 
-         <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
-        <ol type='a'>
-        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
-        <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-        </ol>
-        <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
-    </ol>
+            <ol> 
+            <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
+            <ol type='a'>
+                <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+            </ol>
+            <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+             </ol>
          
     
     Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
